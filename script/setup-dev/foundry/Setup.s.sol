157c6eb837cdf2ab23810ae04b8b628ca412b8a5
qTeePie <isabel@chaparian.no>
2025-12-13 05:49:42 +0100

    pranks transfer of fork azukis

--- script/setup-dev/foundry/Setup.s.sol
+++ script/setup-dev/foundry/Setup.s.sol
@@ -10,42 +10,72 @@ import {BaseDevScript} from "dev-script/BaseDevScript.s.sol";
 import {OrderEngine} from "orderbook/OrderEngine.sol";
 
 // TODO: cryptopunks is not erc721 compatible, custom wrapper l8r?
+// https://docs.openzeppelin.com/contracts/4.x/api/token/erc721
 interface IERC721 {
     function setApprovalForAll(address operator, bool approved) external;
-    function ownerOf(uint256 tokenId) external;
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function transferFrom(address from, address to, uint256 tokenId) external;
 }
 
 contract Setup is BaseDevScript, Config {
     OrderEngine public orderEngine;
 
     function run() external {
+        // --------------------------------
+        // PHASE 0: LOAD CONFIG + SETUP
+        // --------------------------------
+
         _loadConfig("deployments.toml", true);
 
         uint256 chainId = block.chainid;
         console.log("Deploying to chain: %s", chainId);
 
         address azukiAddr = config.get("azuki_addr").toAddress();
-        console.log(azukiAddr);
 
         // select tokens
-        (uint256[] memory ids, uint256 count) = selectTokens(azukiAddr, 10, 2);
+        (uint256[] memory ids) = selectTokens(azukiAddr, 10, 2);
 
         // get number of tokens
         uint256 length = countUntilZero(ids);
+
+        // --------------------------------
+        // PHASE 1: PRANK OWNERS
+        // --------------------------------
+
+        address[] memory owners = new address[](length);
+
         // read owner
+        for (uint256 i = 0; i < length; i++) {
+            owners[i] = readOwnerOf(azukiAddr, ids[i]);
+            console.log("Owner of token %s: %s", ids[i], owners[i]);
+        }
 
         // impersonate each owner
-        /* vm.startBroadcast();
-        // transfer selected tokens to some devAddr
+        for (uint256 i = 0; i < length; i++) {
+            vm.prank(owners[i]);
+            // transfer selected tokens to some devAddr
+            IERC721(azukiAddr).transferFrom(
+                owners[i], // ← ACTUAL OWNER
+                devAddr(1), // ← YOU
+                ids[i]
+            );
+        }
 
-        // fund devAddrs
+        // --------------------------------
+        // PHASE 2: BROADCAST - FUND
+        // --------------------------------
 
-        // impersonate devAddrs and buy WETH
+        // - fund ETH to DEV wallets
+        // - wrap ETH =>  WETH
 
-        // WETH allowance marketplace
+        // --------------------------------
+        // PHASE 3: BROADCAST - APPROVALS
+        // --------------------------------
 
-        //
+        // - WETH allowance to marketplace
+        // - Approve marketplace
 
+        /*
         orderEngine = new OrderEngine();
         vm.stopBroadcast();
 
@@ -62,7 +92,7 @@ contract Setup is BaseDevScript, Config {
         address tokenContract,
         uint256 roof,
         uint8 mod
-    ) internal returns (uint256[] memory, uint256) {
+    ) internal pure returns (uint256[] memory) {
         uint256 count = 0;
         uint256[] memory ids = new uint256[](roof);
 
@@ -74,11 +104,13 @@ contract Setup is BaseDevScript, Config {
                 count++;
             }
         }
-        return (ids, count);
+        return ids;
     }
 
-    function readOwnerOf(address tokenContract, uint256 tokenId) internal {
-        IERC721(tokenContract).ownerOf(tokenId);
-        console.log("Owner of token %s", tokenId);
+    function readOwnerOf(
+        address tokenContract,
+        uint256 tokenId
+    ) internal view returns (address) {
+        return IERC721(tokenContract).ownerOf(tokenId);
     }
 }
